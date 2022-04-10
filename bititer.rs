b450d163bf24f144ef45e13558f1d56c4f8a4985
Bart Massey <bart.massey@gmail.com>
2022-04-10 00:35:39 -0700

    moved bit-order to type/trait

--- bititer.rs
+++ bititer.rs
@@ -1,64 +1,94 @@
+use std::marker::PhantomData;
+
 use num::*;
 
-#[derive(Debug, Clone, Copy)]
-pub enum BitOrder {
-    Msb0,
-    Lsb0,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Msb0;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Lsb0;
+
+pub trait BitOrder {
+    fn indices<T: PrimInt>() -> Box<dyn Iterator<Item = usize>>;
+}
+
+impl BitOrder for Msb0 {
+    fn indices<T: PrimInt>() -> Box<dyn Iterator<Item = usize>> {
+        let tsize = 8 * std::mem::size_of::<T>();
+        Box::new((0..tsize).rev())
+    }
+}
+
+impl BitOrder for Lsb0 {
+    fn indices<T: PrimInt>() -> Box<dyn Iterator<Item = usize>> {
+        let tsize = 8 * std::mem::size_of::<T>();
+        Box::new(0..tsize)
+    }
 }
 
 #[derive(Debug, Clone)]
-pub struct BitIter<T, I> {
-    order: BitOrder,
+pub struct BitIter<T, I, O> {
     cur: T,
     ncur: usize,
     rest: I,
+    order: PhantomData<O>,
 }
 
-impl<T, I> BitIter<T, I>
+impl<T, I, O> BitIter<T, I, O>
 where
     T: PrimInt + std::ops::ShlAssign<usize> + std::ops::ShrAssign<usize>,
     I: Iterator<Item = T>,
+    O: BitOrder,
 {
-    pub fn new(rest: I, order: BitOrder) -> Self {
+    pub fn new(rest: I) -> Self {
         Self {
-            order,
             cur: zero::<T>(),
             ncur: 0,
             rest,
+            order: PhantomData,
         }
     }
 }
 
-
-impl<T, I> Iterator for BitIter<T, I>
+impl<T, I> Iterator for BitIter<T, I, Msb0>
 where
-    T: PrimInt + std::ops::ShlAssign<usize> + std::ops::ShrAssign<usize>,
+    T: PrimInt + std::ops::ShlAssign<usize>,
     I: Iterator<Item = T>,
 {
     type Item = bool;
-    
+
     fn next(&mut self) -> Option<Self::Item> {
         let tsize = 8 * std::mem::size_of::<T>();
         if self.ncur == 0 {
             self.cur = self.rest.next()?;
             self.ncur = tsize;
         }
-        match self.order {
-            BitOrder::Msb0 => {
-                let top = tsize - 1;
-                let mask = one::<T>() << top;
-                let bit = mask & self.cur != num::zero();
-                self.cur <<= 1;
-                self.ncur -= 1;
-                Some(bit)
-            }
-            BitOrder::Lsb0 => {
-                let bit = one::<T>() & self.cur != zero();
-                self.cur >>= 1;
-                self.ncur -= 1;
-                Some(bit)
-            }
+        let top = tsize - 1;
+        let mask = one::<T>() << top;
+        let bit = mask & self.cur != num::zero();
+        self.cur <<= 1;
+        self.ncur -= 1;
+        Some(bit)
+    }
+}
+
+impl<T, I> Iterator for BitIter<T, I, Lsb0>
+where
+    T: PrimInt + std::ops::ShrAssign<usize>,
+    I: Iterator<Item = T>,
+{
+    type Item = bool;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let tsize = 8 * std::mem::size_of::<T>();
+        if self.ncur == 0 {
+            self.cur = self.rest.next()?;
+            self.ncur = tsize;
         }
+        let bit = one::<T>() & self.cur != zero();
+        self.cur >>= 1;
+        self.ncur -= 1;
+        Some(bit)
     }
 }
 
@@ -66,25 +96,34 @@ where
 mod test {
     use super::*;
 
-    fn to_bits<T: PrimInt>(v: &[T], order: BitOrder) -> Vec<bool> {
-        let tsize = 8 * std::mem::size_of::<T>();
+    fn to_bits<T: PrimInt, O: BitOrder>(v: &[T]) -> Vec<bool> {
         let mut result = Vec::new();
-        let iter: Vec<usize> = match order {
-            BitOrder::Lsb0 => (0..tsize).collect(),
-            BitOrder::Msb0 => (0..tsize).rev().collect(),
-        };
         for val in v {
-            for i in &iter {
-                result.push((one::<T>() << *i) & *val != zero::<T>());
+            for i in O::indices::<T>() {
+                result.push((one::<T>() << i) & *val != zero::<T>());
             }
         }
         result
     }
 
-    fn test_one<T>(v: Vec<T>, order: BitOrder)
-    where T: PrimInt + std::ops::ShlAssign<usize> + std::ops::ShrAssign<usize> {
-        let bits = to_bits(&v, order);
-        for (b0, b1) in BitIter::new(v.into_iter(), order).zip(bits.into_iter()) {
+    fn test_one_lsb0<T>(v: Vec<T>)
+    where
+        T: PrimInt + std::ops::ShlAssign<usize> + std::ops::ShrAssign<usize>,
+    {
+        let bits = to_bits::<T, Lsb0>(&v);
+        let vbits = v.into_iter();
+        for (b0, b1) in <BitIter<_, _, Lsb0>>::new(vbits).zip(bits.into_iter()) {
+            assert_eq!(b0, b1);
+        }
+    }
+
+    fn test_one_msb0<T>(v: Vec<T>)
+    where
+        T: PrimInt + std::ops::ShlAssign<usize> + std::ops::ShrAssign<usize>,
+    {
+        let bits = to_bits::<T, Msb0>(&v);
+        let vbits = v.into_iter();
+        for (b0, b1) in <BitIter<_, _, Msb0>>::new(vbits).zip(bits.into_iter()) {
             assert_eq!(b0, b1);
         }
     }
@@ -92,26 +131,22 @@ mod test {
     #[test]
     fn test_bititer() {
         let v: Vec<u8> = Vec::new();
-        test_one(v, BitOrder::Msb0);
+        test_one_lsb0(v);
+        let v: Vec<u8> = Vec::new();
+        test_one_msb0(v);
 
         let r_msb = [false, false, false, true, false, false, true, false];
         let r_lsb = [false, true, false, false, true, false, false, false];
-        let l_msb: Vec<bool> = BitIter::new(
-            vec![0x12_u8].into_iter(),
-            BitOrder::Msb0,
-        ).collect();
-        let l_lsb: Vec<bool> = BitIter::new(
-            vec![0x12_u8].into_iter(),
-            BitOrder::Lsb0,
-        ).collect();
+        let l_msb: Vec<bool> = <BitIter<_, _, Msb0>>::new(vec![0x12_u8].into_iter()).collect();
+        let l_lsb: Vec<bool> = <BitIter<_, _, Lsb0>>::new(vec![0x12_u8].into_iter()).collect();
         assert_eq!(l_msb, r_msb);
         assert_eq!(l_lsb, r_lsb);
 
-        test_one(vec![0x12_u8, 0x34], BitOrder::Msb0);
-        test_one(vec![0x12_u8, 0x34], BitOrder::Lsb0);
-        test_one(vec![0x1234_u16, 0x5678], BitOrder::Msb0);
-        test_one(vec![0x1234_u16, 0x5678], BitOrder::Lsb0);
-        test_one(vec![-0x1234_i16, -0x5678], BitOrder::Msb0);
-        test_one(vec![-0x1234_i16, -0x5678], BitOrder::Lsb0);
+        test_one_msb0(vec![0x12_u8, 0x34]);
+        test_one_lsb0(vec![0x12_u8, 0x34]);
+        test_one_msb0(vec![0x1234_u16, 0x5678]);
+        test_one_lsb0(vec![0x1234_u16, 0x5678]);
+        test_one_msb0(vec![-0x1234_i16, -0x5678]);
+        test_one_lsb0(vec![-0x1234_i16, -0x5678]);
     }
 }
